caf4580151201889090a0d910b4f2f0cc2698189
MoomooKing531x <moomooking531x@gmail.com>
2026-09-13 11:34:17 -0700

    Centralize SHOP_ITEMS in lib/shopConstants and import in shop and locker

--- app/shop/page.js
+++ app/shop/page.js
@@ -3,28 +3,7 @@ import { useState, useEffect } from "react";
 import Navbar from "../../components/Navbar";
 import CoinIcon from "../../components/CoinIcon";
 import AvatarPreview from "../../components/AvatarPreview";
-
-const SHOP_ITEMS = {
-  hats: [
-    { id: "cap", name: "Cap", cost: 1000, filename: "mmk cap.png" },
-    { id: "sports-cap", name: "Sports Cap", cost: 2000, filename: "mmk sports cap.png" },
-    { id: "kid-hat", name: "Kid Hat", cost: 4000, filename: "mmk kid hat.png" },
-    { id: "cowboy-hat", name: "Cowboy Hat", cost: 6000, filename: "mmk cowboy hat.png" },
-    { id: "helmet", name: "Helmet", cost: 14000, filename: "mmk helmet.png" },
-    { id: "top-hat", name: "Top Hat", cost: 20000, filename: "mmktop hat.png" },
-  ],
-  jackets: [
-    { id: "jersey", name: "Jersey", cost: 3000, filename: "mmk jersey.png" },
-    { id: "tuxedo", name: "Tuxedo", cost: 9000, filename: "mmk tuxeto.png" },
-    { id: "leather-jacket", name: "Leather Jacket", cost: 10000, filename: "mmk leather jacket.png" },
-    { id: "armor", name: "Armor", cost: 18000, filename: "mmk armor.png" },
-    { id: "robe", name: "Robe", cost: 30000, filename: "mmk robe.png" },
-  ],
-  accessories: [
-    { id: "gold-neck", name: "Gold Neck", cost: 9000, filename: "gold neck.png" },
-    { id: "sunglasses", name: "Sunglasses", cost: 12000, filename: "mmk sunglasses.png" },
-  ],
-};
+import { SHOP_ITEMS } from "../../lib/shopConstants";
 
 const WELCOME_MESSAGES = [
   "Welcome to the Locker!",
